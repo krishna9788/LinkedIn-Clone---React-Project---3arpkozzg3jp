7adf807b1bde57a6e11761fe94f25b750f04ffab
krishna9788 <krishnakushwaha978@gmail.com>
2023-03-21 16:32:21 +0530

    Project

---
+++ src/mock_backend/UserList.js
@@ -0,0 +1,20 @@
+const userMap = new Map([
+  [
+    "krishnakushwaha978@gmail.com",
+    { id: 1, name: "Krishna Kushwaha", password: "12345678" },
+  ],
+  [
+    "swatisharma@gmail.com",
+    { id: 2, name: "Swati Sharma", password: "newton1234" },
+  ],
+  [
+    "suchitrapatra@gmail.com",
+    { id: 3, name: "Suchitra Patra", password: "12345678" },
+  ],
+  [
+    "anamikagiri@gmail.com",
+    { id: 4, name: "Anamika Giri", password: "newton1234" },
+  ],
+]);
+
+export default userMap;
